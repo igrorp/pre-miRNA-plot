87127fe3d8e6c85173e62b8c6c71a5458f089649
Igor Paim <igorpaim8@gmail.com>
2019-10-21 00:26:02 -0300

    Adding the setup file for installing the Pytho packages

---
+++ setup.py
@@ -0,0 +1,23 @@
+
+from setuptools import setup, find_packages
+
+setup(name='premiRNAplot',
+      version=0.2,
+      description='pre-miRNA secondary structure prediction image generator',
+      author='Igor Paim',
+      author_email='igorpaim8@gmail.com',
+      url='https://github.com/igrorp/pre-miRNA-plot',
+      license='GPL',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Programming Language :: Python :: 3',
+      ],
+      keywords='pre-miRNA',
+      packages=find_packages(),
+      install_requires=[
+          'matplotlib',
+          'scikit-learn',
+          'numpy'
+      ]
+)
