3a9719d95563a4f26b26c31225c18f7ab96a8db4
lgpp <igorpaim8@gmail.com>
2019-10-31 17:12:37 -0300

    Testing new SVG image creation

--- testing/imagetesting.py
+++ testing/imagetesting.py
@@ -35,8 +35,11 @@ class SVGParser():
 		pairs = []
 		transform = None
 		seqtransform = None
+		radius = 0
 
 		self.sequence, self.pairs = self.load_data(file)
+
+		self.radius = self.__radius__()
 
 		if not self.pairs:
 			raise NoPairsAnnotations("Did not find any pairs")
@@ -97,6 +100,27 @@ class SVGParser():
 		return pairs
 
 
+	def __radius__(self):
+
+		x = 0
+		a = []
+
+		while x < len(self.locations) - 1:
+			x1, y1 = self.locations[x]
+			x2, y2 = self.locations[x+1]
+			x+=1
+			a.append(math.sqrt(((y2 - y1) ** 2) + ((x2 - x1) ** 2)))
+
+		n = 0
+
+		for pair in self.pairs:
+			if pair: n+=1
+
+		n = n / len(self.pairs)
+
+		return (st.mean(a) + (st.stdev(a) * n)) / 2
+
+
 	def __box__(self, root):
 
 		''' Returns the width and heigth of the SVG box'''
@@ -125,8 +149,6 @@ class SVGParser():
 		xt = round(float(xyt.split(',')[0][1:]), 5)
 		yt = round(float(xyt.split(',')[1][:-1]), 5)
 
-		print(transform)
-
 		scale = transform[transform.find('scale') + len('scale'):transform.find(' translate')]
 		scalex = round(float(scale.split(',')[0][1:]), 5)
 		scaley = round(float(scale.split(',')[1][:-1]), 5)
@@ -142,7 +164,7 @@ class SVGParser():
 		return sequence, locations
 
 
-some = SVGParser(open('rna2.svg'))
+some = SVGParser(open('rna.svg'))
 
 print(some.box)
 print(some.sequence)
@@ -150,29 +172,28 @@ print(some.pairs)
 print(some.locations)
 print(some.transform)
 
+radius = some.radius
+
+print(f'this is the {radius}')
+
 width, height = some.box
 
+method = 'lines'
 
-dist = 0
-x = 0
-a = []
+fs = radius * 2 * 0.725
 
-while x < len(some.locations) - 1:
-	x1, y1 = some.locations[x]
-	x2, y2 = some.locations[x+1]
-	x+=1
-	a.append(math.sqrt(((y2 - y1) ** 2) + ((x2 - x1) ** 2)))
+fdy = radius / 2
 
-n = 0
+fdx = radius / 2 * -1 * 1.13
 
-for pair in some.pairs:
-	if pair: n+=1
+if method == 'spheres':
+	strokew = radius / 3
+elif method == 'lines':
+	strokew = radius * 2 * 0.725
 
-n = n / len(some.pairs)
+mirpos = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
+mir2pos = [69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91]
 
-radius = (st.mean(a) + (st.stdev(a) * n)) / 2
-
-fs = radius * 0.725
 
 dwg = sw.Drawing('new.svg', viewBox=f"0, 0, {width}, {height}", preserveAspectRatio="xMidYMid meet")
 
@@ -180,9 +201,17 @@ dwg = sw.Drawing('new.svg', viewBox=f"0, 0, {width}, {height}", preserveAspectRa
 
 precursor = dwg.add(dwg.g(id="precursor", transform='translate(0, 10) scale(0.95, 0.95)'))
 
-# Criando o grupo de pares entre nucleotideos conectados
+# Criando todos os grupos
 
-pairsgroup = precursor.add(dwg.g(id='pairs', stroke='black', stroke_width='2'))
+pairsgroup = precursor.add(dwg.g(id='pairs', stroke='black', stroke_width=radius / 6))
+
+circgroup = precursor.add(dwg.g(id='circles', display='none', fill="#9494b8"))
+
+# Adicionando a linha que conecta todos os nucleotideos
+
+precursor.add(dwg.polyline(points=some.locations, stroke='#9494b8', fill="none", stroke_width=strokew))
+
+# Adicionando as linhas que conectam os nucleotídeos pareados
 
 for i in range(len(some.pairs)):
 	if some.pairs[i]:
@@ -190,40 +219,48 @@ for i in range(len(some.pairs)):
 		x2, y2 = some.locations[int(some.pairs[i])]
 		pairsgroup.add(dwg.line(start=some.locations[i], end=some.locations[int(some.pairs[i])])) 
 
-
-# Adicionando a linha que conecta todos os nucleotideos do precursor
-
-precursor.add(dwg.polyline(points=some.locations, stroke='black', fill="none", stroke_width="2"))
-
-# Criando o grupo que vai conter todos os circulos
-
-circgroup = precursor.add(dwg.g(id='circles', fill="#9494b8"))
-
-mirpos = [5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
-mir2pos = [69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91]
-
-textgroup = precursor.add(dwg.g(id='nucleotides', transform=f'translate({-1 * radius / 2 * 1.3},{radius / 2})',font_size=fs, fill='black', font_family='Helvetica', font_weight='bold'))
+# Adicionando a linha do estilo de traço que destaca a linha da posição dos miRNAs
 
 linha1 = []
 linha2 = []
 
 for index, xytuple in enumerate(some.locations):
+
+	if index in mirpos:
+		x, y = some.locations[index]
+		if index == mirpos[0]:
+			y-=radius
+		elif index == mirpos[-1]:
+			y+=radius
+		linha1.append((x,y))
+	elif index in mir2pos:
+		x, y = some.locations[index]
+		if index == mir2pos[0]:
+			y+=radius
+		elif index == mir2pos[-1]:
+			y-=radius
+		linha2.append((x,y))
+
+precursor.add(dwg.polyline(points=linha1, fill='none', stroke='red', stroke_width=strokew))
+precursor.add(dwg.polyline(points=linha2, fill='none', stroke='orange', stroke_width=strokew))
+
+textgroup = precursor.add(dwg.g(id='nucleotides', transform=f'translate({fdx},{fdy})',font_size=fs,
+								fill='black', font_family='Helvetica', font_weight='bold'))
+
+# Adicionando cada um dos círculos do estilo de esferas
+
+for index, xytuple in enumerate(some.locations):
 	
-	circ = circgroup.add(dwg.circle(center=xytuple, r=radius, stroke='none', stroke_width='1.5'))
+	circ = circgroup.add(dwg.circle(center=xytuple, r=radius))
 
 	textgroup.add(dwg.text(some.sequence[index], insert=some.locations[index]))
 	
 	if index in mirpos:
 		circ.fill('red')
-		linha1.append(some.locations[index])
-	if index in mir2pos:
+	elif index in mir2pos:
 		circ.fill('orange')
-		linha2.append(some.locations[index])
-	
 
-#precursor.add(dwg.polyline(points=linha1, fill='red', stroke='red', stroke_width="10"))
-#precursor.add(dwg.polyline(points=linha2, fill='red', stroke='orange', stroke_width="10"))
-
+# Salvando o arquivo com identação
 
 dwg.save(pretty=True)
 
